100262df962e45a3e3113d7fc9b3501fc30cfe32
bamlak21 <bamlakgezahegn27@gmail.com>
2025-10-03 14:12:20 +0300

    feat: Improved Subscription logic
    - Added createOrRenewSubscription function to handle both new
    subscriptions and renewals
    - Generates invoices for new users
    - Extends expiry correctly on renewals without overwriting old records
    - Ensures subscription status is properly set (PENDING/ACTIVE) depending
    on payment

--- src/services/payment.service.ts
+++ src/services/payment.service.ts
@@ -2,10 +2,12 @@ import { Types } from "mongoose";
 import { Context } from "telegraf";
 import { config } from "../config";
 import {
-  CreateSubscription,
-  FindExistingSubscription,
+  createOrRenewSubscription,
+  findExistingSubscription,
+  getSubscription,
 } from "./subscription.service";
 import { Status } from "../types/subscription.type";
+import { findGroup } from "./groups.service";
 
 type Props = {
   ctx: Context;
@@ -15,16 +17,42 @@ type Props = {
   groupId: string;
 };
 
-export const CreateInvoice = async ({
+export const generateInvoice = async (
+  ctx: Context,
+  subscriptionPrice: number,
+  createdSubId: Types.ObjectId
+) => {
+  const priceInCents = Math.round(subscriptionPrice * 100); // chapa accepts amount in cents
+
+  try {
+    const payload = createdSubId.toString();
+    await ctx.replyWithInvoice({
+      title: "Group Sub",
+      description: "Pay to join the group ",
+      payload: payload,
+      provider_token: config.paymentToken,
+      currency: "ETB",
+      prices: [{ label: "Group Access", amount: priceInCents }],
+      start_parameter: "pay",
+      send_phone_number_to_provider: true,
+      need_phone_number: true,
+    });
+  } catch (error) {
+    console.error(error);
+    await ctx.reply(
+      "⚠️ Could not send the payment invoice. Please try again later."
+    );
+  }
+};
+
+export const handleNewSubscription = async ({
   ctx,
   subscriptionPrice,
   userId,
   telegramId,
   groupId,
 }: Props) => {
-  const priceInCents = Math.round(subscriptionPrice * 100); // chapa accepts amount in cents
-
-  const existingSub = await FindExistingSubscription(
+  const existingSub = await findExistingSubscription(
     userId,
     groupId,
     Status.ACTIVE
@@ -34,7 +62,7 @@ export const CreateInvoice = async ({
     return await ctx.reply("✅ You already have an active subscription.");
   }
 
-  const createSub = await CreateSubscription(
+  const createSub = await createOrRenewSubscription(
     groupId,
     userId,
     telegramId,
@@ -44,21 +72,31 @@ export const CreateInvoice = async ({
     return "failed to create Subscription";
   }
 
+  await generateInvoice(ctx, subscriptionPrice, createSub._id);
+};
+
+export const handleRenewalInvoice = async (
+  ctx: Context,
+  subscriptionId: string
+) => {
   try {
-    const payload = createSub._id.toString();
-    await ctx.replyWithInvoice({
-      title: "Group Sub",
-      description: "Pay to join the group ",
-      payload: payload,
-      provider_token: config.paymentToken,
-      currency: "ETB",
-      prices: [{ label: "Group Access", amount: priceInCents }],
-      start_parameter: "pay",
-    });
-  } catch (error) {
-    console.error(error);
-    await ctx.reply(
-      "⚠️ Could not send the payment invoice. Please try again later."
+    const sub = await getSubscription(subscriptionId);
+    if (!sub) return "sub not found";
+    sub.status = Status.Expired;
+    await sub.save();
+    const group = await findGroup(sub.groupId);
+    if (!group) return "Group not found";
+    const createSub = await createOrRenewSubscription(
+      sub.groupId,
+      sub.userId,
+      sub.telegramId,
+      group.subscriptionPrice
     );
+
+    if (!createSub) return "Subscription not Created";
+
+    await generateInvoice(ctx, group.subscriptionPrice, createSub?._id);
+  } catch (error) {
+    console.error("Failed to renew subscription", error);
   }
 };
